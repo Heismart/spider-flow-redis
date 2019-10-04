2133acd7e677b03945b0b30a80b32f1931e4924e
mxd <838425805@qq.com>
2019-10-04 16:40:20 +0800

    重构redis插件

--- src/main/java/org/spiderflow/redis/utils/RedisUtils.java
+++ src/main/java/org/spiderflow/redis/utils/RedisUtils.java
@@ -1,5 +1,6 @@
 package org.spiderflow.redis.utils;
 
+import org.spiderflow.redis.model.RedisSource;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
@@ -10,19 +11,23 @@ import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisUtils {
 	
-	public static StringRedisTemplate createRedisTemplate(String host,int port,String password,int database,int poolMaxActive,int poolMaxIdle,int poolMinIdle){
+	public static StringRedisTemplate createRedisTemplate(String host,Integer port,String password,Integer database,Integer poolMaxActive,Integer poolMaxIdle,Integer poolMinIdle){
 		JedisPoolConfig poolConfig = new JedisPoolConfig();
-		poolConfig.setMaxTotal(poolMaxActive);
-		poolConfig.setMaxIdle(poolMaxIdle);
-		poolConfig.setMinIdle(poolMinIdle);
+		poolConfig.setMaxTotal(poolMaxActive == null ? 8 : poolMaxActive);
+		poolConfig.setMaxIdle(poolMaxIdle == null ? 8 : poolMaxIdle);
+		poolConfig.setMinIdle(poolMinIdle == null ? 0 : poolMinIdle);
 		JedisClientConfiguration jedisConfigConfiguration = JedisClientConfiguration.builder().usePooling().poolConfig(poolConfig).build();
 		RedisStandaloneConfiguration standaloneConfiguration = new RedisStandaloneConfiguration();
-		standaloneConfiguration.setDatabase(database);
+		standaloneConfiguration.setDatabase(database == null ? 0 : database);
 		standaloneConfiguration.setHostName(host);
 		standaloneConfiguration.setPassword(RedisPassword.of(password));
 		standaloneConfiguration.setPort(port);
 		StringRedisTemplate redisTemplate = new StringRedisTemplate(new JedisConnectionFactory(standaloneConfiguration,jedisConfigConfiguration));
 		return redisTemplate;
+	}
+	
+	public static StringRedisTemplate createRedisTemplate(RedisSource redisSource){
+		return createRedisTemplate(redisSource.getHost(), redisSource.getPort(), redisSource.getPassword(), redisSource.getDbIndex(), redisSource.getMaxConnections(), redisSource.getMaxIdle(), redisSource.getMinIdle());
 	}
 
 }
--- src/main/java/org/spiderflow/redis/web/RedisController.java
+++ src/main/java/org/spiderflow/redis/web/RedisController.java
@@ -1,16 +1,30 @@
 package org.spiderflow.redis.web;
 
+import java.util.List;
+
+import org.spiderflow.common.CURDController;
+import org.spiderflow.executor.PluginConfig;
 import org.spiderflow.model.JsonBean;
+import org.spiderflow.model.Plugin;
+import org.spiderflow.redis.mapper.RedisSourceMapper;
+import org.spiderflow.redis.model.RedisSource;
+import org.spiderflow.redis.service.RedisSourceService;
 import org.spiderflow.redis.utils.RedisUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
 @RestController
 @RequestMapping("/redis")
-public class RedisController {
+public class RedisController extends CURDController<RedisSourceService,RedisSourceMapper, RedisSource> implements PluginConfig{
 	
+	@Autowired
+	private RedisSourceService redisSourceService;
+
 	@RequestMapping("/test")
 	public JsonBean<String> test(String host,Integer port,String password,Integer database){
 		try {
@@ -28,5 +42,14 @@ public class RedisController {
 			return new JsonBean<String>(-1,e.getMessage());
 		}
 	}
+	
+	@RequestMapping("/all")
+	public JsonBean<List<RedisSource>> all(){
+		return new JsonBean<List<RedisSource>>(redisSourceService.list(new QueryWrapper<RedisSource>().select("id","name").orderByDesc("createDate")));
+	}
 
+	@Override
+	public Plugin plugin() {
+		return new Plugin("Redis数据源", "redis.html");
+	}
 }
